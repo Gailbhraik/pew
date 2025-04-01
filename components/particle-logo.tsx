148e1a4e50767763949a87baefd8903f7a2fae83
Gailbhraik <57574442+Gailbhraik@users.noreply.github.com>
2025-04-01 17:39:02 +0200

    fix: change area to value_area in particles density configuration

--- components/particle-logo.tsx
+++ components/particle-logo.tsx
@@ -80,7 +80,7 @@ export function ParticleLogo() {
             number: {
               density: {
                 enable: true,
-                area: 800,
+                value_area: 800,
               },
               value: 40,
             },
